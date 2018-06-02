b3a05fd8bb84baece7c245f5bd5a50037d9d6b1e
Jonathan <lee.jonathanw@gmail.com>
2018-06-02 18:52:05 -0500

    Setup Auth0

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'OC5JBWrQ2BfLHm7RX2d3S88opMc8l1hM',
   domain: 'agilentinnovations.auth0.com',
-  callbackUrl: 'http://localhost:8080/callback',
+  callbackUrl: 'http://127.0.0.1:8080/callback',
   apiUrl: 'API_IDENTIFIER'
 }
