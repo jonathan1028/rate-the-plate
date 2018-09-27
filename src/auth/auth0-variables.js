e7c005d288a4775500d05258c4980dbfdd44291f
Jonathan <lee.jonathanw@gmail.com>
2018-09-26 20:06:10 -0500

    Test 2

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -3,5 +3,6 @@ export const AUTH_CONFIG = {
   domain: 'agilentinnovations.auth0.com',
   callbackUrl: 'http://127.0.0.1:8080/callback',
   // callbackUrl: 'http://pennypincher.agilentinnovations.com/callback',
+  callbackUrl: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8080/callback' : 'https://pennypincher.agilentinnovations.com/callback',
   apiUrl: 'API_IDENTIFIER'
 }
